a4a1c38036e9001ce415ee330e2e1ca6856180aa
Tjesto <szycha6@gmail.com>
2015-09-10 23:07:10 +0200

    Changes

--- src/com/mszychowiak/experimental/startcv/OnRecognitionTaskListener.java
+++
@@ -1,8 +0,0 @@
-package com.mszychowiak.experimental.startcv;
-
-import java.util.List;
-import java.util.Map;
-
-public interface OnRecognitionTaskListener {
-	void onExtractionFinished(List<Map<String, String>> result);
-}
